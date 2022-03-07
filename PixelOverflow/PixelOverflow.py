f1e8c288e1d5c9ae8011f2be00677fdebc8a1f5f
Maria Chobanu <marychobanu@gmail.com>
2022-03-07 13:49:39 +0400

    add line fitting for pixels overflow

--- PixelOverflow/PixelOverflow.py
+++ PixelOverflow/PixelOverflow.py
@@ -1,5 +1,5 @@
 ﻿from numpy.random import default_rng
-rng = default_rng()
+rng = default_rng(seed = 22)
 import os
 from astropy.io import fits
 import matplotlib.pyplot as plt
@@ -13,6 +13,32 @@ def get_random_pixel(number_of_pixels):
     for i in range(number_of_pixels):
         coords.append( (rng.integers(0, 500), rng.integers(0, 500)) )
     return coords
+
+def plot_scatter(x, y):
+    plt.title(f'Зависимость пикселя с координатами {c} от экспозиции')
+    plt.scatter(x, y, s=3)
+    plt.savefig(f'Пиксель {c}.png')
+    plt.clf()
+
+def take_only_non_negative(x, y):
+    x_non_negative = []
+    y_non_negative = []
+    for (x_i, y_i) in zip(x, y):
+        if y_i >= 0:
+            x_non_negative.append(x_i)
+            y_non_negative.append(y_i)
+    return (x_non_negative, y_non_negative)
+
+def plot_scatter_and_fit(x, y):
+    (x_non_negative, y_non_negative) = take_only_non_negative(x, y)
+    m, b = np.polyfit(x_non_negative, y_non_negative, 1)
+    plt.title(f'Зависимость пикселя с координатами {c} от экспозиции')
+    plt.scatter(x, y, s=3)
+    plt.plot(x, m*x + b, 'm-', linewidth=0.8, label=f'{m:.2f} x + {b:.2f}')
+    plt.legend()
+    plt.savefig(f'Пиксель {c}_fit.png')
+    plt.clf()
+
 
 plt.figure()
 exposure_and_intensities = []
@@ -35,10 +61,9 @@ for subdir, dirs, files in os.walk(fits_dir):
 
 #convert to numpy array to use slices:
 exposure_and_intensities = np.array(exposure_and_intensities)
+
 for (j, c) in zip(range(16), coords):
     exposure = exposure_and_intensities[:,j,0]
-    intensitys = exposure_and_intensities[:,j,1]
-    plt.title(f'Зависимость пикселя с координатами {c} от экспозиции')
-    plt.scatter(exposure, intensitys, s=3)
-    plt.savefig(f'Пиксель {c}.png')
-    plt.clf()
+    intensities = exposure_and_intensities[:,j,1]
+    plot_scatter(exposure, intensities)
+    plot_scatter_and_fit(exposure, intensities)
