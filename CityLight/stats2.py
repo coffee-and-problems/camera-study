4764ecf7dd3b6345b60a1874129cff891dfe8013
Maria Chobanu <marychobanu@gmail.com>
2022-05-07 13:08:47 +0300

    c

--- CityLight/stats2.py
+++ CityLight/stats2.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 import datetime
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import FK5
 
 def dms2dd(s):
     degrees, minutes, seconds = re.split(' ', s)
@@ -21,58 +24,47 @@ def to_time2(s):
     return datetime.datetime.strptime(s, "%H:%M")
 
 tab = pd.read_fwf("TABOB.TXT")
-ra = {}
-dec = {}
+coords = {}
 
 for index, row in tab.iterrows():
-    ra[row[0]] = row[2]
-    dec[row[0]] = f"{row[3]} {row[4]}"
+    ra = float(row[1])*15 + float(row[2])*0.25 + float(row[3])*1/240
+    dec = float(row[4]) + float(row[5])/60 + float(row[6])/60/60
 
-a = pd.read_csv("pointing.dat", header=None, delimiter=' ')
-names = a.iloc[:,2]
+    fk5c = SkyCoord(ra*u.degree, dec*u.degree, frame='fk5')
+    fk5_2022 = FK5(equinox='B2022')
+    coords[row[0]] = fk5c.transform_to(fk5_2022)
 
-def sub(a):
-    split1 = a.split(' ')
-    h = float(split1[0])*60*60 + float(split1[1])*60 + float(split1[2])
-    hours, remainder = divmod(h, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    return hours*15 + minutes*0.25 + seconds*1/240
-
-def sub2(a, b, c):
-    split1 = a.split(' ')
-    split_time = c.split(':')
-    h = float(split1[0])*15 + float(split1[1])*0.25 + float(split1[2])*1/240 - b #+ float(split_time[0])*15 + float(split_time[1])*0.25 + float(split_time[2])*1/240
-    return h
+data = pd.read_csv("pointing.dat", header=None, delimiter=' ')
 
 x_a = []
 x_d = []
 y_a = []
 y_d = []
-for index, row in a.iterrows():
+for index, row in data.iterrows():
     try:
-        x_a.append(sub(ra[row[2]]))
-        x_d.append(dms2dd(dec[row[2]]))
-        y_a.append(sub2(ra[row[2]], float(row[3]), row[0].split(";")[-1]))
-        y_d.append(dms2dd2(dec[row[2]]) - float(row[4])*60*60)
+        x_a.append(coords[row[2]].ra.degree)
+        x_d.append(coords[row[2]].dec.degree)
+        y_a.append(coords[row[2]].ra.degree*60 - float(row[3])*60)
+        y_d.append(coords[row[2]].dec.degree*60 - float(row[4])*60)
     except:
         continue
 
-a = pd.DataFrame({'x_a': x_a,
+df = pd.DataFrame({'x_a': x_a,
                   'y_a': y_a})
 
-a = a.sort_values(by=['x_a'])
+df = df.sort_values(by=['x_a'])
 plt.grid(True)
 plt.grid(linestyle=':')
-plt.scatter(a['x_a'], a['y_a'], color='b', marker='o', s=8)
+plt.scatter(df['x_a'], df['y_a'], color='b', marker='o', s=8)
 plt.xticks(rotation=90, ha='right')
 plt.xlabel('прямое восхождение, градусы')
-plt.ylabel('поправка, градусы')
+plt.ylabel('поправка, угловые минуты')
 plt.show()
 
 plt.grid(True)
 plt.grid(linestyle=':')
 plt.scatter(x_d, y_d, c='b', marker='o', s=8)
 plt.xlabel('склонение, градусы')
-plt.ylabel('поправка, угловые секунды')
+plt.ylabel('поправка, угловые минуты')
 plt.show()
 
